9d70c2fca7415a8193360ac89a13410ab41d42a9
J.R. Ruggiero <jrruggiero@shift3tech.com>
2021-02-11 13:50:38 -0800

    chore(data): location info

---
+++ data/locations.js
@@ -0,0 +1,136 @@
+const LocationInfo = {
+    cathedralOpen: {
+        title: 'Opening Cathedral Fight',
+        time: '600'
+    },
+    cathedralFinish: {
+        title: 'Finish Cathedral [Yakra]',
+        time: '600'
+    },
+    guardia600Queen: {
+        title: 'Guardia Castle - Queens Quarters',
+        time: '600'
+    },
+    zenanBridge: {
+        title: 'Zenan Bridge [Zombor]',
+        time: '600'
+    },
+    desert: {
+        title: 'Sunken Desert [Retenite]',
+        time: '600'
+    },
+    denadoroMt: {
+        title: 'Denadoro Mts. [Masa & Mune]',
+        time: '600'
+    },
+    frogLoft: {
+        title: 'Frogs Loft',
+        time: '600'
+    },
+    forgeMasamune: {
+        title: 'Forge Masamune (Melchior)',
+        time: '1000'
+    },
+    turnInMasamune: {
+        title: 'Turn in Masamune',
+        time: '600'
+    },
+    magusCastle: {
+        title: 'Magus Castle (Unlock Dark Ages) [Magus]',
+        time: '600'
+    },
+    carpenter: {
+        title: 'Capenter (Choras)',
+        time: '1000'
+    },
+    snailStop: {
+        title: 'Snail Stop (Porre)',
+        time: '1000'
+    },
+    heckranCave: {
+        title: 'Heckran Cave [Heckran]',
+        time: '1000'
+    },
+    tabanTurnIn: {
+        title: 'Taban Reward',
+        time: '1000'
+    },
+    giantsClaw: {
+        title: "Giant's Claw [Rust Tyrano]",
+        time: '1000'
+    },
+    pendantTrial: {
+        title: 'Pendant Trial (Unlock Future) [Dragon Tank]',
+        time: '1000'
+    },
+    prismTrial: {
+        title: 'Guardia Castle King Trial [Yakra XIII]',
+        time: '1000'
+    },
+    sunKeep: {
+        title: 'Sun Keep [Son of Sun]',
+        time: '2300'
+    },
+    protoDome: {
+        title: 'Proto Dome Character',
+        time: '2300'
+    },
+    protoDomeEoT: {
+        title: 'Proto Dome Gate (Unlock EoT)',
+        time: '2300'
+    },
+    genoDome: {
+        title: 'Geno Dome [Mother Brain]',
+        time: '2300'
+    },
+    factory: {
+        title: 'Factory [R66 Series]',
+        time: '2300'
+    },
+    arrisDome: {
+        title: 'Arris Dome [Guardian]',
+        time: '2300'
+    },
+    mysticHouse: {
+        title: 'Mystic Residence (Unlock EoT)',
+        time: '1000'
+    },
+    spekkio: {
+        title: 'Learn Magic',
+        time: 'EoT'
+    },
+    sunStone: {
+        title: 'Forge Sunstone - Guardia Castle',
+        time: '1000'
+    },
+    dactylNest: {
+        title: 'Dactyl Nest',
+        time: 'prehistory'
+    },
+    reptiteLair: {
+        title: 'Reptitle Lair [Nizbel]',
+        time: 'prehistory'
+    },
+    tyranoLair: {
+        title: 'Tyrano Lair [Black Tyrano]',
+        time: 'prehistory'
+    },
+    mtWoe: {
+        title: 'Mt. Woe [Giga Gaia]',
+        time: 'prehistory'
+    },
+    deathPeak: {
+        title: 'Death Peak [Lavos Spawn]',
+        time: '2300'
+    },
+    blackOmen: {
+        title: 'The Black Omen',
+        time: 'dark'
+    },
+    oceanPalace: {
+        title: 'Ocean Palace',
+        time: 'dark'
+    }
+};
+
+export default LocationInfo;
